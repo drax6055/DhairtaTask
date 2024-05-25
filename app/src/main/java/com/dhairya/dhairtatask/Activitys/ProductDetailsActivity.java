6071b454e470170e42c71add0b2293cad7fab6f9
Dhairya <drax6055@gmail.com>
2024-05-25 00:57:33 -1200

    done

---
+++ app/src/main/java/com/dhairya/dhairtatask/Activitys/ProductDetailsActivity.java
@@ -0,0 +1,45 @@
+package com.dhairya.dhairtatask.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dhairya.dhairtatask.Model.Product;
+import com.dhairya.dhairtatask.R;
+import com.squareup.picasso.Picasso;
+
+public class ProductDetailsActivity extends AppCompatActivity {
+    private TextView title, description, price, category, brand, stock, rating;
+    private ImageView thumbnail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+
+        title = findViewById(R.id.productTitle);
+        description = findViewById(R.id.productDescription);
+        price = findViewById(R.id.productPrice);
+        category = findViewById(R.id.productCategory);
+        brand = findViewById(R.id.productBrand);
+        stock = findViewById(R.id.productStock);
+        rating = findViewById(R.id.productRating);
+        thumbnail = findViewById(R.id.productThumbnail);
+
+        // Get the product object from the intent
+        Product product = (Product) getIntent().getSerializableExtra("product");
+
+        // Set the product details to the views
+        if (product != null) {
+            title.setText(product.getTitle());
+            description.setText(product.getDescription());
+            price.setText(String.format("$%.2f", product.getPrice()));
+            category.setText(product.getCategory());
+            brand.setText(product.getBrand());
+            stock.setText(String.valueOf(product.getStock()));
+            rating.setText(String.valueOf(product.getRating()));
+            Picasso.get().load(product.getThumbnail()).into(thumbnail);
+        }
+    }
+}
